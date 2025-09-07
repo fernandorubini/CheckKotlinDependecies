19ba2828128c10ab6cae9c86bc464fe4f8dcf19d
fernandorubini <fernandotestelealapps@gmail.com>
2025-09-07 13:44:45 -0300

    test: add ExampleJUnit5Test (JUnit 5)

---
+++ app/src/test/java/com/fernandodev/checkkotlindependecies/ExampleJUnit5Test.kt
@@ -0,0 +1,11 @@
+package com.fernandodev.checkkotlindependecies
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ExampleJUnit5Test {
+    @Test
+    fun 2 + 2 deve ser 4() {
+        assertEquals(4, 2 + 2)
+    }
+}
